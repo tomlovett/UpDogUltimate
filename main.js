b243d8e956548919653b0089c48e018886ecf648
Tom Lovett <tomlovett6@gmail.com>
2016-01-22 16:14:01 -0700

    pre-loading team

--- main.js
+++ main.js
@@ -5,109 +5,137 @@ angular.module('UpDog')
 
 /* initial values */
 /* -------------- */
-		$scope.us = 0;
-		$scope.them = 0;
-		$scope.substitutionMode = true;
+	$scope.us = 0;
+	$scope.them = 0;
+	$scope.substitutionMode = true;
 
-		$scope.undoHistory = [];
+	$scope.undoHistory = [];
 
-		$scope.pointHistory = [];
+	$scope.pointHistory = [];
 
 /* object models */
 /* ------------- */
-		$scope.point = {
-			pulling: 	null, // bool
-			playersOn:  null, // array
-			weScored: 	null, // bool
-			time: 		null, // number
-		};
+	$scope.point = {
+		pulling: 	null, // bool
+		playersOn:  null, // array
+		weScored: 	null, // bool
+		time: 		null, // number
+	};
 
-		$scope.player = {
-			name: 		'',
-			gender: 	'',
-			handle: 	'',
+	$scope.player = function(name, gender, handle) {
+		return {
+			name: 		name,
+			gender: 	gender,		// 'm' or 'f'
+			handle: 	(handle || name),
 			game: 		[0, 0, 0],
 			history: 	[0, 0, 0],
- 			linkages: 	{}
-		};
+			linkages: 	{}
+		}
+	};
 
-		$scope.linkage = {
-			players: 	['player1', 'player2'],
-			game: 		[0, 0, 0],
-			history: 	[0, 0, 0]
-		};
+	$scope.linkage = function(player1, player2) {
+		players: 	[player1, player2],
+		game: 		[0, 0, 0],
+		history: 	[0, 0, 0]
+	};
 
-		$scope.team = {
-			men: 		[],
-			women: 		[],
+	$scope.team = function() {
+		return {
+			roster: 	[],
 			bench: 		[],
 			field: 		[]
-		};
+		}
+	};
+
+	var genTeam = function(roster) {
+		var team = $scope.team();
+		for (player in roster) {
+			genLinkages(player, $scope.team.roster);
+			team.roster.push(player);
+			team.bench.push(player);
+			if (player.gender == 'm') {
+				team.men.push(player);
+			} else {
+				team.women.push(player);
+			}
+		}
+	}
+
+	var genLinkages = function(player, bench) {
+		for (teammate in bench) {
+			var link = $scope.linkage(player, teammate);
+			player.linkages[teammate.name] = link;
+			teammate.linkages[player.name] = link;
+		}
+	}
 
 /* recording score */
 /* --------------- */
-		$scope.recordScore = function(weScored) {
-			var val;
-			if (weScored) {
-				$scope.us += 1
-				val = 1;
-			} else {
-				$scope.them += 1
-				val = -1;
+	$scope.recordScore = function(weScored) {
+		var score;
+		if (weScored) {
+			$scope.us += 1
+			score = 1;
+		} else {
+			$scope.them += 1
+			score = -1;
+		}
+		scorePlayers(score);
+		scoreTeamLinkages(score);
+		$scope.substitutionMode = true;
+	}
+
+	var scorePlayers = function(score) {
+		for (player in $scope.team.field) {
+				markScore(player, score)
 			}
-			scorePlayers(val);
-			scoreTeamLinkages(val);
-			$scope.substitutionMode = true;
-		}
+	}
 
-		var scorePlayers = function(val) {
-			for (player in $scope.team.field) {
-					markScore(player, val)
-				} 
-		}
+	var markScore = function(player, score) {
+		player.game[0] += score;
+		player.game[1] += score;
+		player.history[0] += score;
+		player.history[1] += score;
+	}
 
-		var markScore = function(player, score) {
-			player.game[0] += score;
-			player.game[1] += score;
-			player.history[0] += score;
-			player.history[1] += score;
-		}
-
-		var scoreTeamLinkages = function(weScored) {
-			var length = $scope.team.field.length
-			for (var x = 0; x < length-1; x++) {
-				var player1 = $scope.team.field[x];
-				for (var y = 1; y < length; y++) {
-					var player2 = $scope.team.field[y];
-
-				}
+	var scoreTeamLinkages = function(score) {
+		var length = $scope.team.field.length
+		for (var x = 0; x < length-1; x++) {
+			var player1 = $scope.team.field[x];
+			for (var y = 1; y < length; y++) {
+				var player2 = $scope.team.field[y].name;
+				player1.linkages[player2] += score;
 			}
 		}
-
-		var scoreLinkage = function(player1, player2, val)
+	}
 
 /* handling substitutions */
 /* ---------------------- */
-		$scope.doneSubstitutions = function() {
-			$scope.substitutionMode = false;
-		}
-
-		$scope.subOn = function(player) {
-			var index = $scope.team.bench.indexOf(player)
-			$scope.team.bench.splice(index, 1)
-			$scope.team.field.push()
-		}
-
-		$scope.subOff = function(player) {
-			var index = $scope.team.field.indexOf(player)
-			$scope.team.field.splice(index, 1)
-			$scope.team.bench.push()		}
-
-		$scope.clearLine = function() {
-			for (player in $scope.team.field) {
-				$scope.subOff(player);
-			}
-		}
-
+	$scope.doneSubstitutions = function() {
+		$scope.substitutionMode = false;
 	}
-])
+
+	$scope.subOn = function(player) {
+		var index = $scope.team.bench.indexOf(player)
+		$scope.team.bench.splice(index, 1)
+		$scope.team.field.push()
+		// sub in a way that players are inherently sorted?
+	}
+
+	$scope.subOff = function(player) {
+		var index = $scope.team.field.indexOf(player)
+		$scope.team.field.splice(index, 1)
+		$scope.team.bench.push()		}
+
+	$scope.clearLine = function() {
+		for (player in $scope.team.field) {
+			$scope.subOff(player);
+		}
+	}
+
+	darkSide = {
+		
+	}
+
+
+}])
