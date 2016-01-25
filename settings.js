1bcc211e54bf1ad11e29e443bf31daa61b0c1e6c
Tom Lovett <tomlovett6@gmail.com>
2016-01-25 15:17:59 -0700

    switching views functional

--- settings.js
+++ settings.js
@@ -2,28 +2,18 @@ angular.module('settings', []);
 
 angular.module('settings')
 	.controller('settings', ['$scope', function($scope) {
-		$scope.players = true;
-		$scope.teams = false;
-		$scope.game = false;
 
-		$scope.masterKey = [$scope.players, $scope.teams, $scope.game];
-
-		$scope.viewPlayers = function() {
-			if ($scope.players == true) { 
-				return;
-			} else {
-				$scope.masterKey = [true, false, false];
-			}
-		}
-
+		$scope.viewKey = [true, false, false]; // players, team, game settings
+	
 		$scope.changeView = function(index) {
 			console.log('changeView fired. key: ', index);
-			if ($scope.masterKey[index] == true) {
+			if ($scope.viewKey[index] == true) {
 				return;
 			} else {
-				$scope.masterKey = [false, false, false];
-				$scope.masterKey[index] = true;
+				$scope.viewKey = [false, false, false];
+				$scope.viewKey[index] = true;
 			}
+			console.log($scope.viewKey);
 		}
 
 	}])
