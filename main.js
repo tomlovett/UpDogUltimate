406d65ce4239e1527b61585bad1d0a46c7671dc0
Tom Lovett <tomlovett6@gmail.com>
2016-01-29 14:32:13 -0700

    init'd player creation form

--- main.js
+++ main.js
@@ -11,14 +11,17 @@ UpDog.controller('gameManager', ['$scope', 'utility', function($scope, utility) 
 		$scope.score = function(result) {
 			$scope.game.recordPoint(result);
 			$scope.subMode = true;
+			console.log($scope.game);
 		} 
 
 		$scope.doneSubbing = function() {
 			$scope.subMode = false;
-			console.log('game: ', $scope.game);
 			$scope.logOnField();
+			$scope.game.currentPoint.time = Date.now();
+			console.log($scope.game);
 		}
 
+// could combine team.men.field and team.women.field with a pointer array
 		$scope.logOnField = function() {
 			$scope.team.men.field.forEach(function(player) {
 				$scope.game.currentPoint.playersOn.push(player);
@@ -54,12 +57,42 @@ UpDog.controller('gameManager', ['$scope', 'utility', function($scope, utility) 
 
 }]);
 
+UpDog.controller('settings', ['$scope', 'utility', function($scope, utility) {
+	$scope.player = {};
+	$scope.playerErrors = { name: false, gender: false };
+
+	$scope.submitPlayer = function() {
+		$scope.playerErrors = $scopeVerifyInput()
+		if ($scope.playerErrors.name || $scope.playerErrors.gender) {
+			return;
+		}
+		var player = new Player($scope.player.name, $scope.player.gender, $scope.player.handle);
+		console.log(player);
+		$scope.player = {};
+	}
+
+	$scope.verifyInput = function() {
+		var result = [false, false];
+		if (!player.name) {
+			result[0] = true;
+		}
+		if (!player.gender.match(/[fm]/)) {
+			result[1] = true;
+		}
+		return result;
+	}
+
+}]);
+
 UpDog.factory('utility', function() {
+
+// cross-recording data is a little less than intuitive, but otherwise leads to less code
+
 // Player \\
 	 function Player(name, gender, handle) {
 		this.name   =  name;
 		this.gender =  gender;
-		this.handle =  (handle || name);
+		this.handle =  handle || name;
 		this.game   =  [0, 0, 0];
 		this.career =  [0, 0, 0];
 		this.points =  []
@@ -67,7 +100,7 @@ UpDog.factory('utility', function() {
 
 	Player.prototype = {
 		markPoint : function(result) {
-			this.game[0]   += result; // eh, point or not
+			this.game[0]   += result;
 			this.game[1]   += 1;
 			this.career[0] += result;
 			this.career[1] += 1;
@@ -75,42 +108,32 @@ UpDog.factory('utility', function() {
 	}
 
 // Team \\
+// this.game?
 	function Team(name) {
-		this.name  = name;
+		this.name  = name || 'US';
 		this.men   = { bench: [], field: [] }
 		this.women = { bench: [], field: [] }
 	}
 
 	Team.prototype = {
 		constructor: Team,
-		sortFunc : function(a, b) {
-			if (a.handle > b.handle) {
-				return 1;
-			} else if (a.handle < b.handle) {
-				return -1;
-			} else {
-				return 0;
-			}
-		},
 		sort : function() {
-			this.men.bench.sort(this.sortFunc);
-			this.men.field.sort(this.sortFunc);
-			this.women.bench.sort(this.sortFunc);
-			this.women.field.sort(this.sortFunc);
+			this.men.bench = _.sortBy(this.men.bench, 'handle');
+			this.men.field = _.sortBy(this.men.field, 'handle');
+			this.women.bench = _.sortBy(this.women.bench, 'handle');
+			this.women.field = _.sortBy(this.women.field, 'handle');
 		},
 		addToRoster : function(player) {
-			if (player.gender == 'm') {
-				this.men.bench.push(player);
-			} else {
-				this.women.bench.push(player);
-			}
+			if (player.gender == 'm') { this.men.bench.push(player); }
+			else { this.women.bench.push(player); }
 			this.sort()
 		}
 	}
 
 // Game \\
+// this.team?
 	function Game() {
-		this.score = [0,0];
+		this.score = [0, 0];
 		this.pointHistory = [];
 		this.currentPoint = new Point(1);
 	}
@@ -119,17 +142,14 @@ UpDog.factory('utility', function() {
 		constructor: Game,
 		recordPoint: function(result) {
 			this.updateScoreboard(result)
-			this.currentPoint.recordResult(result)
+			this.currentPoint.recordResult(result);
 			this.pointHistory.push(this.currentPoint);
 			this.updatePlayerPoints(this.currentPoint);
 			this.currentPoint = new Point(result);
 		},
 		updateScoreboard: function(result) {
-			if (result == 1) {
-				this.score[0] += 1;
-			} else {
-				this.score[1] += 1;
-			} 
+			if (result == 1) { this.score[0] += 1; }
+			else { this.score[1] += 1; } 
 		},
 		updatePlayerPoints: function(point) {
 			point.playersOn.forEach(function(player) {
@@ -140,19 +160,24 @@ UpDog.factory('utility', function() {
 
 // Point \\
 	function Point(pulling) {
-		this.pulling   = pulling;
-		this.time 	   = Date.now();
+		this.pulling   = pulling;		// 1 or -1
+		this.time 	   = undefined;
 		this.playersOn = [];
-		this.result    = undefined
+		this.result    = undefined;		// 1 or -1
 	}
 
 	Point.prototype = {
 		constructor: Point,
 		recordResult: function(result) {
 			this.result = result;
+			this.recordTime();
 			this.playersOn.forEach(function(player) {
 				player.markPoint(result);
 			});
+		},
+		recordTime: function() {
+			this.time = Date.now() - this.time;
+			this.time = (this.time / 1000); // convert to mins & seconds
 		}
 	}
 /* pre-loads */
@@ -180,8 +205,6 @@ UpDog.factory('utility', function() {
 		person = new Player(person.name, person.gender);
 		darkSide.addToRoster(person)
 	});
-
-	darkSide.sort()
 
 	return {
 		Player 	 : Player,
